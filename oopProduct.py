ff39ccba3d97cbe084af6719cdfad1caa973e6f2
Samantha Rabling <sjarillorabling@gmail.com>
2018-03-10 15:16:44 -0800

    finished product, but there's one thing that still needs to work on

--- oopProduct.py
+++ oopProduct.py
@@ -9,35 +9,44 @@ class Product(object):
     
     def selling(self):
         self.status = 'Sold'
-        return self.status
+        return self
+
+        
 
     def addTax(self):
         self.tax = self.price * .08
         return self.tax
 
-    def returnedItem(self, reason, box):
-        if reason == 'defective':
+    def returnedItem(self, reason):
+        if "defective" in reason:
             self.price = 0
-            self.status = 'defective'
-        elif reason == 'returning':
-            if box == 'closed':
-                self.status = 'For Sale'
-        if reason == 'returning':
-            if box == 'opened':
-                self.status = 'Used'
-                self.price = price - (price * .20)
-    
+            self.status = "defective"
+        elif "returning_close" in reason:
+            self.status = "For Sale"
+        elif "returning_open" in reason:
+            self.status = "Used"
+            self.price = price - (price * .20)
+        else:
+            "For Sale" in self.status 
+        return self
+
+        self.returnedItem('new')
+        self.selling()   
 
     def displayInfo(self):
-        self.addTax()
-        print "Price: " + str(self.price)
-        print "Item Name: " + str(self.item_name)
-        print "Weight: " + str(self.weight)
-        print "Brande: " + str(self.brand)
-        print "Status:" + str(self.status)
-        print "Tax: " + str(self.tax)
+        print("Name: {}, Price: ${}, Weight: {}, Brand: {}, Status: {}, Tax: ${}".format(
+        self.item_name, self.price, self.weight, self.brand, self.status, self.addTax()))
+    
+        
+        # print "Price: " + str(self.price)
+        # print "Item Name: " + str(self.item_name)
+        # print "Weight: " + str(self.weight)
+        # print "Brand: " + str(self.brand)
+        # print "Status:" + str(self.status)
+        # print "Tax: " + str(self.tax)
 
-prod1 = Product(40, 'name', '1lb', 'Fancy Brand', 'defective' )
+prod1 = Product(40, 'name', '1lb', 'Fancy Brand', 'new')
+
 # prod2 = 
 # prod3 = 
 # prod4 = 
