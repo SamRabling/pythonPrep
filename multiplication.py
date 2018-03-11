fb6d77b66d500aa9f470f95832e1422edb3b96b4
Samantha Rabling <sjarillorabling@gmail.com>
2018-03-10 16:16:59 -0800

    got the main task, but may want to go back for the formating

--- multiplication.py
+++ multiplication.py
@@ -1,8 +1,9 @@
-side = 1
-top = [1,2,3,4,5,6,7,8,9,10,11,12]
-row = []
-j = 0
-print str(top)[1:-1]
-for i in range(0,len(top)):
-    j = (top[i] * i)
-print j
+from __future__ import print_function
+
+num = [1,2,3,4,5,6,7,8,9,10,11,12,13]
+# print str([1,2,3,4,5,6,7,8,9,10,11,12])[1:-1]
+for i in range(1, len(num)):
+    print (i, end=' ')
+    for j in range(1, len(num)):
+        print ((i * j), end=' ')
+print ()
